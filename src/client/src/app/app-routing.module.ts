2637da7b120bb8cfc9be8278aec988cec4d0b06b
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-27 07:05:22 -0500

    feat(client): add routing path for sign-in

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { PageUsersComponent } from './pages/page-users/page-users.component';
 
 const routes: Routes = [
-  {path: 'users', component: PageUsersComponent}
+  {path: 'users', component: PageUsersComponent},
+  {path: 'signIn', component: SignInComponent}
 ];
 
 @NgModule({
