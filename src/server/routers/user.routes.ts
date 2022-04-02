dbfa26a62df87adbd9cfe1b9fbff872e219e5fed
Aman <amandeepk221@gmail.com>
2022-04-02 11:58:59 -0700

    feat(server): create routers and move user http request from server to user routes

---
+++ src/server/routers/user.routes.ts
@@ -0,0 +1,104 @@
+import express from "express";
+import bcrypt from 'bcrypt';
+import { UserModel } from "../schemas/user.schema.js";
+
+const userRouter = express.Router();
+
+// Salt and Hash for password encryption
+var saltRounds = 10;
+//var password = "Fkdj^45ad"
+
+userRouter.get('/users', function(req,res){
+    UserModel.find()
+    .then(data => res.json({data}))
+    .catch(err => {
+        res.status(501)
+        res.json({errors: err});
+    })
+});
+
+userRouter.post('/create-user', function(req,res){
+    const {name, email, username, password} = req.body;
+    // salt and hash orignial password to encrypted password
+    bcrypt.genSalt(saltRounds, function(err,salt) {
+        console.log("salt = ",salt);
+        bcrypt.hash(password,salt,function(err,hash) {
+            console.log("Hash = " + hash);
+            // store hash in database here
+            const user = new UserModel({
+                name,
+                username,
+                email,
+                password: hash // pass password as type hash, it will assign hashed password generated to password property
+            });
+            user
+            .save()
+            .then((data) => {
+                res.json({data});
+            })
+            .catch(err => {
+                res.status(501);
+                res.json({errors: err});
+            })
+        })
+    
+    });
+    
+});
+
+//login user(sign in)
+userRouter.post('/login', function(req,res) {
+    const {username,password} = req.body;
+
+    UserModel
+    .findOne({username})
+    .then((user:any) => {
+        if(user == null)
+        {
+            console.log("Username not found");
+            res.json({message: "Username not found!"});
+            return;
+        }
+        console.log("username found : ",user);
+        bcrypt.compare(password, `${user?.password}`, function(err, result) {
+            if(result) {
+                console.log("It matches!");
+                res.json({message: "Successfully logged in!"});
+            }
+            else {
+                console.log("Invalid Password");
+                res.sendStatus(403)
+            }
+        })
+    })
+    .catch(err => res.json({err}))
+})
+
+userRouter.delete('/delete-user/:id', function(req, res) {
+    const _id = req.params.id;
+    UserModel.findByIdAndDelete(_id).then((data) => {
+        console.log(data);
+        res.json({data});
+    });
+})
+
+userRouter.put('/update-user/:id', function(req, res) {
+    console.log("Update user");
+    UserModel.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: { name: req.body.name, email: req.body.email },
+        },
+        {
+            new: true,
+        },
+        function(err, updateUser) {
+            if(err) {
+                res.send("Error updating user");
+            }
+            else{
+                res.json(updateUser);
+            }
+        }
+    )
+})
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,5 @@
-
 import express from 'express';
-import bcrypt from 'bcrypt';
 import cors from 'cors';
-import { UserModel } from './schemas/user.schema.js'
 import mongoose from 'mongoose';
 
 const app = express();
@@ -15,12 +12,6 @@ mongoose.connect('mongodb://localhost:27017/test')
 })
 .catch(err => console.log('Failed to Connect to DB', err))
 
-// Salt and Hash for password encryption
-var saltRounds = 10;
-//var password = "Fkdj^45ad"
-
-
-
 // cors- for browser
 app.use(cors());
 app.use(express.json());
@@ -28,100 +19,6 @@ app.use(express.json());
 app.get('/', function(req, res) {
    res.json({message:'test'});
 });
-
-app.get('/users', function(req,res){
-    UserModel.find()
-    .then(data => res.json({data}))
-    .catch(err => {
-        res.status(501)
-        res.json({errors: err});
-    })
-});
-app.post('/create-user', function(req,res){
-    const {name, email, username, password} = req.body;
-    // salt and hash orignial password to encrypted password
-    bcrypt.genSalt(saltRounds, function(err,salt) {
-        console.log("salt = ",salt);
-        bcrypt.hash(password,salt,function(err,hash) {
-            console.log("Hash = " + hash);
-            // store hash in database here
-            const user = new UserModel({
-                name,
-                username,
-                email,
-                password: hash // pass password as type hash, it will assign hashed password generated to password property
-            });
-            user
-            .save()
-            .then((data) => {
-                res.json({data});
-            })
-            .catch(err => {
-                res.status(501);
-                res.json({errors: err});
-            })
-        })
-    
-    });
-    
-});
-
-//login user(sign in)
-app.post('/login', function(req,res) {
-    const {username,password} = req.body;
-
-    UserModel
-    .findOne({username})
-    .then((user:any) => {
-        if(user == null)
-        {
-            console.log("Username not found");
-            res.json({message: "Username not found!"});
-            return;
-        }
-        console.log("username found : ",user);
-        bcrypt.compare(password, `${user?.password}`, function(err, result) {
-            if(result) {
-                console.log("It matches!");
-                res.json({message: "Successfully logged in!"});
-            }
-            else {
-                console.log("Invalid Password");
-                res.sendStatus(403)
-            }
-        })
-    })
-    .catch(err => res.json({err}))
-})
-
-app.delete('/delete-user/:id', function(req, res) {
-    const _id = req.params.id;
-    UserModel.findByIdAndDelete(_id).then((data) => {
-        console.log(data);
-        res.json({data});
-    });
-})
-
-app.put('/update-user/:id', function(req, res) {
-    console.log("Update user");
-    UserModel.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: { name: req.body.name, email: req.body.email },
-        },
-        {
-            new: true,
-        },
-        function(err, updateUser) {
-            if(err) {
-                res.send("Error updating user");
-            }
-            else{
-                res.json(updateUser);
-            }
-        }
-    )
-})
 
 
 app.listen(PORT, function(){
